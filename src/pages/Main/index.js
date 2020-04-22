79748136da571e88e85f81c053436ce5bc54e998
Fabio Ribeiro Martins <5039966-fabiormdkp@users.noreply.gitlab.com>
2020-04-22 16:11:01 -0300

    Card e menu

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import Header from '~/components/Header'
-import Tabs from '~/components/Tabs'
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
-import { Container } from './styles';
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-export default function Main(){
+import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
 
-  return(
+export default function Main() {
+
+  return (
     <Container>
       <Header />
+      <Content>
+        <Menu />
+
+        <PanGestureHandler>
+          <Card>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Diego Schell Fernandes hoje às 06:00
+            </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+      </Content>
+
       <Tabs />
     </Container>
   );
